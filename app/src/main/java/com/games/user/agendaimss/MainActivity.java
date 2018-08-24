d0eea52c0d52d13a509fa3bdc3d4a6ea5fa84758
po0oncho0o9208 <35234062+po0oncho0o9208@users.noreply.github.com>
2018-08-24 02:08:08 -0500

    cambios

--- app/src/main/java/com/games/user/agendaimss/MainActivity.java
+++ app/src/main/java/com/games/user/agendaimss/MainActivity.java
@@ -1,16 +1,22 @@
 package com.games.user.agendaimss;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -50,16 +56,21 @@ public class MainActivity extends ListActivity {
         //setListAdapter(adapter);
         //ListView listView = getListView();
         //Drawable res = getResources().getDrawable(R.drawable.btnagregar);
-
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int ancho = metrics.widthPixels / (100) * 20;
+        int anchobtn = metrics.widthPixels / (100) * 28;
         ArrayList<Category> category = new ArrayList<Category>();
         for (int i = 0; i < values.size(); i++) {
 
             String paths = Environment.getExternalStorageDirectory() +
-                    File.separator + RUTA_IMAGEN + File.separator + 0 + values.get(i).fecha + ".jpg";
+                    File.separator + RUTA_IMAGEN + File.separator + 0 + values.get(i).fecha + values.get(i).motivo + values.get(i).horas + ".jpg";
 
             Bitmap bitmap = BitmapFactory.decodeFile(paths);
+
             Drawable d = new BitmapDrawable(getResources(), bitmap);
-            category.add(new Category("olo" + values.get(i).id, "Servicio 1", values.get(i).fecha + "-" + '\n' + values.get(i).motivo, d));
+
+
+            category.add(new Category("olo" + values.get(i).id, "Servicio 1", values.get(i).fecha + "-" + values.get(i).motivo, getResizedBitmap(bitmap, ancho, anchobtn)));
         }
         ListView listView = findViewById(android.R.id.list);
         AdapterCategory adapter = new AdapterCategory(this, category);
@@ -95,5 +106,33 @@ public class MainActivity extends ListActivity {
         // ArrayAdapter<Contact> adapter = new ArrayAdapter<Contact>(this, android.R.layout.simple_expandable_list_item_1, values);
         //setListAdapter(adapter);
     }
-}
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            startActivity(new Intent(getBaseContext(), MenuPrincipal.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+}
